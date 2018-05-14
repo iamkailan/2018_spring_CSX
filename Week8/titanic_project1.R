5d2882dac81038bcfa40d1db3760b2fc572f11c4
imkailan <r05630002@ntu.edu.tw>
2018-05-14 10:16:14 +0800

    proj1

--- Week8/titanic_project1.R
+++ Week8/titanic_project1.R
@@ -36,23 +36,21 @@ Fareothers <- median(train_test[train_test$Pclass=='3' & train_test$Embarked=='S
                      na.rm = TRUE)
 Fareothers
 train_test$Fare[faremiss] <- 8.00
-ggplot(train_test[1:891,], aes(x = Fare, color = factor(Survived))) +
+ggplot(train_test[1:309,], aes(x = Fare, color = factor(Survived))) +
   geom_line(stat='count', position='dodge') +
   theme_few()
 embarkedmiss <- which(train_test$Embarked=="")
 train_test[embarkedmiss,]
-embark_fare <- train_test %>% #去除有缺值的行
-  filter(PassengerId != 62 & PassengerId != 830)
+
 ggplot(embark_fare, aes(x = Embarked, y = Fare, fill = factor(Pclass))) +
   geom_boxplot() +
   geom_hline(aes(yintercept=80), 
              colour='red', linetype='dashed', lwd=1) +
   scale_y_continuous(labels=dollar_format()) +
   theme_few()
-train_test$Embarked[c(62, 830)] <- 'C'
 train_test$Embarked <- factor(train_test$Embarked)
 train_test$familysize <- train_test$SibSp + train_test$Parch +1
-ggplot(train_test[1:891,], aes(x = familysize, fill = factor(Survived))) +
+ggplot(train_test[1:309,], aes(x = familysize, fill = factor(Survived))) +
   geom_bar(stat='count', position='dodge') +
   scale_x_continuous(breaks=c(1:11)) +
   labs(x = 'Family Size') +
